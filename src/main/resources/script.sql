ade21f23ba85705c338264cba05e4d381c81a8e6
ghost1497 <rfrankie04@gmail.com>
2018-03-06 17:14:41 -0500

    missed the rating on line 17

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -14,7 +14,7 @@ SELECT * FROM movies WHERE genre = 'Sci-Fi';
 
 SELECT * FROM movies WHERE imdb_score >= 6.5;
 
-SELECT * FROM movies WHERE runtime < 100;
+SELECT * FROM movies WHERE rating = 'G' or rating = 'PG' AND runtime < 100;
 
 SELECT AVG(runtime) as avg_runtime, genre FROM movies WHERE imdb_score < 7.5 GROUP BY genre;
 
