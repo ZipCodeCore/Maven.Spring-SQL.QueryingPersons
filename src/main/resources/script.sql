8de56e0d0ab4d1a9ae47236b9b1d30712e36be53
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-06 19:09:31 -0500

    simulation complete

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -10,10 +10,34 @@ Values ('Scarface'), ('Mean Girls'), ('shrek'), ('Shrek2'), ('Shrek');
 
 SELECT * From movies WHERE GENRE = 'Sci-Fi';
 SELECT * From movies WHERE IMDB_SCORE >= 6.5;
+SELECT * From movies WHERE Runtime < 100 AND RATING IN ('G', 'PG');
+OR
+SELECT * From movies WHERE Runtime < 100 AND (RATING = 'G' OR RATING = 'PG');
+
+SELECT GENRE, AVG(RUNTIME)
+FROM movies
+WHERE IMDB_SCORE < 7.5
+GROUP BY GENRE
+;
+
+UPDATE movies SET RATING = 'R' WHERE TITLE = 'Starship Troopers';
+SELECT ID, TITLE, RATING from MOVIES WHERE GENRE IN('Horror', 'Documentary')
+OR
+SELECT ID, TITLE, RATING from MOVIES WHERE GENRE= 'Horror' OR GENRE = 'Documentary';
+
+SELECT RATING, AVG(IMDB_SCORE), MAX(IMDB_SCORE), MIN(IMDB_SCORE)
+from MOVIES GROUP BY RATING;
+
+//before i could figure out how to use having workaround:
+SELECT COUNT(*), RATING, AVG(IMDB_SCORE), MAX(IMDB_SCORE), MIN(IMDB_SCORE)
+from MOVIES GROUP BY RATING ;
+
+SELECT COUNT(*), RATING, AVG(IMDB_SCORE), MAX(IMDB_SCORE), MIN(IMDB_SCORE)
+from MOVIES GROUP BY RATING HAVING COUNT(*) > 1;
 
 
+DELETE from MOVIES WHERE RATING = 'R';
 
-SELECT * From movies;
 
+//SELECT * From movies;
 
-//SELECT From movies WHERE genre = 'Sci-Fi';
